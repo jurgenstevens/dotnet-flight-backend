59bc2a5bbf0f1e815c573b7fd37d5e6e029fab61
jurgenstevens <jurgen.stevens@yahoo.com>
2024-07-11 00:08:32 -0500

    Clean up the GetGame string into a var.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using DotNetFlights.Api.Dtos;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+const string GetFlightEndpointName = "GetFlight";
+
 List<FlightDto> flights = [
   new (1, "United", "ORD", 1234, new DateOnly(2025, 6, 21), "A2", "Carbonara"),
   new (2, "Southwest", "AUS", 4321, new DateOnly(2024, 8, 25), "B8", "Cacio e Pepe"),
@@ -16,7 +18,7 @@ List<FlightDto> flights = [
 app.MapGet("flights", () => flights);
 
 // GET http://localhost:5157/flights/flightId
-app.MapGet("flights/{flightId}", (int flightId) => flights.Find(flight => flight.Id == flightId)).WithName("GetFlight");
+app.MapGet("flights/{flightId}", (int flightId) => flights.Find(flight => flight.Id == flightId)).WithName(GetFlightEndpointName);
 
 // POST http://localhost:5157/flights
 app.MapPost("flights", (CreateFlightDto newFlight) => 
@@ -33,7 +35,7 @@ app.MapPost("flights", (CreateFlightDto newFlight) =>
 
   flights.Add(flight);
 
-  return Results.CreatedAtRoute("GetFlight", new { flightId = flight.Id}, flight);
+  return Results.CreatedAtRoute(GetFlightEndpointName, new { flightId = flight.Id}, flight);
 });
 
 app.Run();
