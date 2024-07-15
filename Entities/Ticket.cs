ebd90e93abb135645331f28324c32f093acf4d21
jurgenstevens <jurgen.stevens@yahoo.com>
2024-07-15 18:26:35 -0500

    Include flight and flight id in the Ticket entity file.

--- Entities/Ticket.cs
+++ Entities/Ticket.cs
@@ -1,8 +1,12 @@
-﻿namespace DotNetFlights.Api;
+﻿using DotNetFlights.Api.Entities;
+
+namespace DotNetFlights.Api;
 
 public class Ticket
 {
   public string TicketId { get; set;}
   public required string Seat { get; set;}
   public required decimal Price { get; set;}
+  public int FlightId { get; set;}
+  public Flight Flight { get; set;} = null!;
 }
--- obj/Debug/net8.0/DotNetFlights.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotNetFlights.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetFlights.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f356a47dabf603e03ba5ca7c6340642a4178f5ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ee6ca71cbd1a190db2de3fc0ff10167dd53022d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetFlights.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetFlights.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
