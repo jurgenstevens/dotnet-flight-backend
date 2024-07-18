6971449e2627858ecee8c28b7ea193ea66f6d24c
jurgenstevens <jurgen.stevens@yahoo.com>
2024-07-17 23:13:47 -0500

    Connect existing Flight and Ticket entities with navigation properties.

--- Entities/Ticket.cs
+++ Entities/Ticket.cs
@@ -7,6 +7,6 @@ public class Ticket
   public string TicketId { get; set;}
   public required string Seat { get; set;}
   public required decimal Price { get; set;}
-  public int FlightId { get; set;}
-  public Flight Flight { get; set;} = null!;
+  public int FlightId { get; set;} // Required foreign key property
+  public Flight Flight { get; set;} = null!; // Required reference navigation to principal
 }
